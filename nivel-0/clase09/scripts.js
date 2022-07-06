f99b6b12b1e3e14f3375a8be36f5985317f9c9d2
TENZI SARACENI Carlos Daniel <daniel.tenzi@soprasteria.com>
2022-07-06 15:23:02 +0200

    mas lecciones

--- nivel-0/clase09/scripts.js
+++ nivel-0/clase09/scripts.js
@@ -1,23 +1,4 @@
-function ejemploIF(){
-
-    var literal = document.getElementById('input1').value;
-
-    if (literal){
-        alert("Has escrito algo en la casilla de texto, es lo que sigue: "+ literal);
-    } else {
-        alert("no has escrito nada y la casilla de texto está vacía");
-    }
-}
-
-function ejemploSwitch(){
-    var literal = document.getElementById('input1').value;
-    
-    // isNaN() comprueba si una variable es No Numérica. Quiere decir que devolverá TRUE para cualquier cosa
-    // que no sea un número. Por ejemplo: isNaN('hola!') => true, isNaN('100') => false
-
-    if (!isNaN(literal)){
-        alert("Has escrito algo en la casilla de texto, es lo que sigue: "+ literal);
-    } else {
-        alert("no has escrito nada y la casilla de texto está vacía");
-    }
+function saludar(){
+    var nombre = document.getElementById('inputNombre').value;
+    alert("Hola "+ nombre + "!");
 }
